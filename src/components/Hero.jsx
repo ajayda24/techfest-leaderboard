c8c6086a3ec025f59d583479086abcd3d864600c
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2026-03-14 13:40:03 +0530

    Added glow card for Hero section"

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import Card3D from "./Card3D";
+
+export default function Hero() {
+  return (
+    <div className="flex w-full justify-center">
+      <Card3D content={"CS/IT"} description={"1200 Points"} />
+    </div>
+  );
+}
